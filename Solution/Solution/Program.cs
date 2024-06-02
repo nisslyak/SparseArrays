aafc53ac267615fee9d5977fad8a281233269689
nisslyak <nissopodolyak@gmail.com>
2024-06-02 06:09:41 +0300

    First commit

---
+++ Solution/Solution/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static List<int> matchingStrings(List<string> strings, List<string> queries)
+    {
+        var results = new int[queries.Count];
+        for (int i = 0; i < queries.Count; i++)
+        {
+            results[i] = strings.Count(s => s == queries[i]);
+        }
+        return results.ToList();
+    }
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            int stringsCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<string> strings = new List<string>();
+
+            for (int i = 0; i < stringsCount; i++)
+            {
+                string stringsItem = Console.ReadLine();
+                strings.Add(stringsItem);
+            }
+
+            int queriesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<string> queries = new List<string>();
+
+            for (int i = 0; i < queriesCount; i++)
+            {
+                string queriesItem = Console.ReadLine();
+                queries.Add(queriesItem);
+            }
+
+            List<int> res = Result.matchingStrings(strings, queries);
+        }
+    }
+
+}
